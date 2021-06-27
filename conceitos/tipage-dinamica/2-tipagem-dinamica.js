6e4176e5abd94b95cf5a42a7195de9e52b0826a4
Murillo ROmeu <murillosi@gmail.com>
2021-06-26 21:56:03 -0300

    comentarios

--- conceitos/tipage-dinamica/2-tipagem-dinamica.js
+++ conceitos/tipage-dinamica/2-tipagem-dinamica.js
@@ -1,6 +1,6 @@
 // É possível alterar o tipo da variável depois da sua declaração
-var minhaVariavel = 30;
+var minhaVariavel = 30; //number
 
-minhaVariavel = 'Texto';
+minhaVariavel = 'Texto'; //string
 
 console.log(minhaVariavel);
