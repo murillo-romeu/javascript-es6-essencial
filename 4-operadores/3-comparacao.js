4f77598369bc44936974092ca62c8024a8204d26
Murillo ROmeu <murillosi@gmail.com>
2021-06-27 20:21:44 -0300

    operadores comparacao

---
+++ 4-operadores/3-comparacao.js
@@ -0,0 +1,36 @@
+// ifual ==
+// retorna verdadeiro caso os operando possuam o mesmo conteudo
+"3" == 3
+3 == var1
+
+// diferente
+// retorna verdadeiro caso os operados não sejam iguais
+var2 != 4
+
+// estritamente igual
+// retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo
+3 === 3
+
+// estritamente diferente
+// retorna verdadeiro caso os operandos sejam diferentes e/ou não seja do mesmo tipo
+3 !== 4
+3 !== '3'
+
+// maior que
+// retorna verdadeiro caso o operando esquerdo seja maior que o direito
+4 > 3
+
+// menor que
+// retorna verdadeiro caso o operando esquerdo seja menor que o direito
+2 < 3
+
+// maior que ou igual
+// retorna verdadeiro caso o operando esquerdo seja maior ou igual ao direito
+3 >= 2
+3 >= 3
+
+// menor que ou igual
+// retorna verdadeiro caso o operando esquerdo seja menor ou igual ao direito
+3 <= 2
+3 <= 3
+
