b662f5b7251aa7f6d29aa26ef67c0b5ac3fdec9b
Murillo ROmeu <murillosi@gmail.com>
2021-06-27 20:11:14 -0300

    operadores aritmeticos

---
+++ 4-operadores/1-aritmeticos.js
@@ -0,0 +1,29 @@
++ // soma
+- // subtração
+/ // divisão
+* // multiplicação
+
+// Módulo - retorna o resto da divisão
+12 % 5 
+
+// incremente
+++x // incrementa antes
+x++ // incrementa depois
+
+// decremento
+--x
+x--
+
+// negação (-) adição (+)
+-3
++'3'  // retorna 3
++true // retorna 1
++false// retorna 0
+-true // retorna -1
+
+// operador de exponenciação (**)
+2 ** 3 // dois elevecado ao cubo - retorna 8
+10 ** -1 // retorna 0.1
+
+// oeprador de agurpamento ( )
+2 * (3 + 2)
