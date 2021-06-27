8b72f88daf5b3b3609ededdc0e174be70f533d85
Murillo ROmeu <murillosi@gmail.com>
2021-06-26 22:39:30 -0300

    escopo variaveis

---
+++ 2-variaveis/1-declaracao.js
@@ -0,0 +1,7 @@
+var nameVar = 'Murillo';
+let nameLet = 'Murillo';
+const nameConst = 'Murillo';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);
