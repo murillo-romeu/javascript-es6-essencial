46649d4f3ddb9ad4f694c045847ab268ada8da5f
Murillo ROmeu <murillosi@gmail.com>
2021-07-14 23:01:01 -0300

    criar array

---
+++ 9-manipulacao-iteracao-arrays/1-criar-array.js
@@ -0,0 +1,15 @@
+const persons = Array.of('John', 'Cris', 'Paul');
+console.log(persons);
+
+const numberAndStrings = Array.of(1, 2, 'Text');
+console.log(numberAndStrings);
+
+const arrayEmpty = Array(10)
+console.log(arrayEmpty);
+
+const numbers = Array(1, 2, 3)
+console.log(numbers);
+
+// const divs = document.querySelectorAll('div');
+// console.log(divs);
+//const divArray = Array.from(divs)
